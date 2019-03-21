b6462a4ac34b917085506306682e53de82177051
Sean Kelley <sean@305spin.com>
2019-03-21 16:51:44 -0600

    setting up basic theme and chats view

---
+++ client/scripts/lib/app.js
@@ -0,0 +1,39 @@
+// Libs
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
+import { Meteor } from 'meteor/meteor';
+ 
+// Modules
+import ChatsCtrl from '../controllers/chats.controller';
+import CalendarFilter from '../filters/calendar.filter';
+import RoutesConfig from '../routes';
+ 
+const App = 'Thatssapp';
+ 
+// App
+Angular.module(App, [
+	'angular-meteor',
+	'ionic'
+]);
+
+new Loader(App)
+	.load(ChatsCtrl)
+	.load(CalendarFilter)
+	.load(RoutesConfig);
+
+// Startup
+if (Meteor.isCordova) {
+	Angular.element(document).on('deviceready', onReady);
+}
+else {
+	Angular.element(document).ready(onReady);
+}
+ 
+function onReady() {
+	Angular.bootstrap(document, [App]);
+}
---
+++ client/scripts/routes.js
@@ -0,0 +1,28 @@
+import { Config } from 'angular-ecmascript/module-helpers';
+ 
+import chatsTemplateUrl from '../templates/chats.html';
+import tabsTemplateUrl from '../templates/tabs.html';
+ 
+export default class RoutesConfig extends Config {
+	configure() {
+		this.$stateProvider
+		.state('tab', {
+			url: '/tab',
+			abstract: true,
+			templateUrl: tabsTemplateUrl
+		})
+		.state('tab.chats', {
+			url: '/chats',
+			views: {
+				'tab-chats': {
+					templateUrl: chatsTemplateUrl,
+					controller: 'ChatsCtrl as chats'
+				}
+			}
+		});
+
+		this.$urlRouterProvider.otherwise('tab/chats');
+	}
+}
+ 
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
